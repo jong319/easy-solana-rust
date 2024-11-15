0dfbd0f2fa53ffaf6c641dfbbddc26259c80f20e
jong319 <jonasong319@gmail.com>
2024-11-15 18:16:50 +0800

    feature: added token name, ticker and uri to easy solana details

--- src/reader/easy_solana_account.rs
+++ src/reader/easy_solana_account.rs
@@ -12,11 +12,10 @@ use spl_token::state::{
 };
 
 use crate::{
-    error::AccountReaderError,
     constants::solana_programs::{
         system_program,
         token_program
-    },
+    }, error::AccountReaderError, get_metadata_of_token, get_metadata_of_tokens
 };
 
 /// Convenience interpretation of a basic solana account, containing all the important variables and information.
@@ -65,6 +64,9 @@ pub struct AssociatedTokenAccountDetails {
 /// Convenience interpretation of a Mint account 
 #[derive(Debug)]
 pub struct MintAccountDetails {
+    pub token_name: String,
+    pub token_ticker: String,
+    pub token_uri: String,
     /// Total supply of tokens.
     pub supply: u64,
     /// Number of base 10 digits to the right of the decimal place.
@@ -91,7 +93,6 @@ pub fn get_easy_solana_account(client: &RpcClient, pubkey: &Pubkey) -> Result<Ea
     } else if account.owner == system_program() {
         AccountType::Wallet
     } else if account.owner == token_program() {
-        // Try to unpack as TokenAccount or MintAccount
         if let Ok(associated_token_account) = AssociatedTokenAccount::unpack(&account.data) {
             AccountType::AssociatedTokenAccount(AssociatedTokenAccountDetails {
                 mint_address: associated_token_account.mint,
@@ -101,7 +102,18 @@ pub fn get_easy_solana_account(client: &RpcClient, pubkey: &Pubkey) -> Result<Ea
                 close_authority: associated_token_account.close_authority.into()
             })
         } else if let Ok(mint_account) = Mint::unpack(&account.data) {
+            let mut token_name = String::new();
+            let mut token_ticker = String::new();
+            let mut token_uri = String::new();
+            if let Ok(token_metadata) = get_metadata_of_token(client, pubkey) {
+                token_name = token_metadata.data.name;
+                token_ticker = token_metadata.data.symbol;
+                token_uri = token_metadata.data.uri;
+            }
             AccountType::MintAccount(MintAccountDetails {
+                token_name,
+                token_ticker,
+                token_uri,
                 supply: mint_account.supply,
                 decimals: mint_account.decimals,
                 is_initialized: mint_account.is_initialized,
@@ -129,14 +141,12 @@ pub fn get_easy_solana_account(client: &RpcClient, pubkey: &Pubkey) -> Result<Ea
 /// Fetches and parses accounts given a slice of Pubkeys, returning a Vec<EasySolanaAccount>. 
 /// Invalid accounts are removed. If RPC client fails to fetch data, returns a `AccountReaderError`
 pub fn get_easy_solana_accounts(client: &RpcClient, pubkeys: &[Pubkey]) -> Result<Vec<EasySolanaAccount>, AccountReaderError> {
-    // Fetch multiple accounts
+    // Step 1: Fetch multiple accounts
     let accounts_result = client.get_multiple_accounts(pubkeys);
+    let accounts = accounts_result.map_err(AccountReaderError::from)?; // handle error
 
-    // Handle errors in fetching accounts
-    let accounts = accounts_result.map_err(AccountReaderError::from)?;
-
-    // Parse accounts into EasySolanaAccount
-    let easy_accounts: Vec<EasySolanaAccount> = accounts
+    // Step 2: Parse accounts into EasySolanaAccount
+    let mut easy_accounts: Vec<EasySolanaAccount> = accounts
         .into_iter()
         .zip(pubkeys)
         .filter_map(|(account_option, pubkey)| {
@@ -158,6 +168,9 @@ pub fn get_easy_solana_accounts(client: &RpcClient, pubkeys: &[Pubkey]) -> Resul
                         })
                     } else if let Ok(mint_account) = Mint::unpack(&account.data) {
                         AccountType::MintAccount(MintAccountDetails {
+                            token_name: String::new(),
+                            token_ticker: String::new(),
+                            token_uri: String::new(),
                             supply: mint_account.supply,
                             decimals: mint_account.decimals,
                             is_initialized: mint_account.is_initialized,
@@ -183,6 +196,30 @@ pub fn get_easy_solana_accounts(client: &RpcClient, pubkeys: &[Pubkey]) -> Resul
             })
         })
         .collect();
+
+    // Step 3: Filter for Vector of mint pubkeys to query metadata in a bulk request
+    let mint_pubkeys: Vec<Pubkey> = easy_accounts
+    .iter()
+    .filter_map(|account| match account.account_type {
+        AccountType::MintAccount(_) => Some(account.pubkey),
+        _ => None,
+    })
+    .collect();
+
+    if !mint_pubkeys.is_empty() {
+        let metadata_accounts = get_metadata_of_tokens(client, &mint_pubkeys)?;
+
+        for metadata_account in metadata_accounts {
+            if let Some(account) = easy_accounts.iter_mut().find(|acc| acc.pubkey == metadata_account.mint) {
+                if let AccountType::MintAccount(details) = &mut account.account_type {
+                    details.token_name = metadata_account.data.name.clone();
+                    details.token_ticker = metadata_account.data.symbol.clone();
+                    details.token_uri = metadata_account.data.uri.clone();
+                }
+            }
+        }
+    }
+
     Ok(easy_accounts)
 }
 
@@ -203,6 +240,25 @@ mod tests {
     const CLOSED_ACCOUNT_ADDRESS: &str = "7o2B9chozpRvHsLgm1Qp3UV9NrS7bx7NH3BZKSePtHEh";
     // Invalid Address
     const INVALID_ADDRESS: &str = "thisisaninvalidaddress";
+
+    #[test]
+    fn test_get_easy_solana_account() {
+        let pubkey = HAPPY_CAT_MINT_ADDRESS.parse::<Pubkey>().unwrap();
+        let client = create_rpc_client("RPC_URL");
+        let easy_solana_account = get_easy_solana_account(&client, &pubkey).expect("Failed to fetch accounts");
+        assert!(easy_solana_account.sol_balance > 0.0); // all accounts need lamports for rent
+        // solana addresses are 32 bytes long
+        assert!(easy_solana_account.pubkey.to_bytes().len() == 32); 
+        assert!(easy_solana_account.owner.to_bytes().len() == 32);
+        match easy_solana_account.account_type {
+            AccountType::MintAccount(details) => {
+                assert!(details.decimals == 6);
+                assert!(details.token_name == "HAPPY");
+                assert!(details.token_ticker == "Happy Cat");
+            }
+            _ => {}
+        }
+    }
 
     #[test]
     fn test_get_easy_solana_accounts() {
@@ -239,7 +295,8 @@ mod tests {
                 }
                 AccountType::MintAccount(details) => {
                     assert!(details.decimals == 6);
-                    println!("{:?}", details);
+                    assert!(details.token_name == "HAPPY");
+                    assert!(details.token_ticker == "Happy Cat");
                 }
                 _ => {}
             }
